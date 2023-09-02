54a50369fd83726c3bb7386c9dbf88b8153058c3
teru-0529 <andrea.pirlo.0529@gmail.com>
2023-09-02 09:49:41 +0900

    わざとテスト失敗してみる

--- mux_test.go
+++ mux_test.go
@@ -30,7 +30,7 @@ func TestNewMux(t *testing.T) {
 	if err != nil {
 		t.Fatalf("faited to read body: %v", err)
 	}
-	want := `{"status": "ok"}`
+	want := `{"status": " ok"}`
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
